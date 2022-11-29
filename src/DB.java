1ae260b093c7f3cf822ff9580a1d894f1e39cc21
Niels Kochert <48764692+vescito@users.noreply.github.com>
2022-11-29 11:12:16 +0100

    DB klasse

---
+++ src/DB.java
@@ -0,0 +1,87 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class DB {
+	private PreparedStatement ps = null;
+	private Connection con = null;
+	private int counter_prepared = 1;
+
+	public DB() {
+	}
+
+	public DB(String db, String user, String pass) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				String kommando =
+						"jdbc:mysql://localhost/" + Zugangsdaten.db +
+						"?user=" + Zugangsdaten.user +
+						"&password=" Zugangsdaten.pass;
+				con = DriverManager.getConnection(kommando);
+			} catch(Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("Der DB_Zugang ist nicht vorhanden!");
+			}
+		}
+
+	public void close() {
+		finalize();
+	}
+
+	@Override
+	public void finalize() {
+		try {
+			ps.close();
+		} catch (SQLException e) {
+		}
+		ps = null;
+		try {
+			con.close();
+		} catch (SQLException e) {
+		}
+		con = null;
+	}
+
+	public void setSQL(String sql) {
+		try {
+			ps = con.prepareStatement(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("SQL-Fehler: " + e.getMessage());
+		}
+	}
+
+	public ArrayList<LinkedHashMap<String, String>> lesenJava() {
+		try {
+			return konvertiereJava(ps.executeQuery());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("DB lesenJava: " + e.getMessage());
+		}
+	}
+
+	public ArrayList<LinkedHashMap<String, String>> konvertiereJava(ResultSet rs) throws SQLException {
+
+		ArrayList<LinkedHashMap<String, String>> daten = new ArrayList<>();
+		int anz_spalten = rs.getMetaData().getColumnCount();
+		if (anz_spalten == 0)
+			return daten;
+		while (rs.next()) {
+			LinkedHashMap<String, String> datensatz = new LinkedHashMap<>();
+			for (int i = 1; i < anz_spalten; i++) {
+				String name = rs.getMetaData().getColumnName(i);
+				String wert = rs.getString(name);
+				if (wert != null)
+					datensatz.put(name, wert);
+				else
+					datensatz.put(name, "");
+			}
+			daten.add(datensatz);
+		}
+		return daten;
+	}
+}
